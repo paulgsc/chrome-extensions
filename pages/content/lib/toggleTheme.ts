e52eaa3bfa86371e2db15cc2e5c1a32aeeac37c4
Paul Gathondu <aulgondu@gmail.com>
2024-07-03 22:29:18 -0700

    refactor: add tabs fn to content script for test

--- pages/content/lib/toggleTheme.ts
+++ pages/content/lib/toggleTheme.ts
@@ -1,7 +1,10 @@
+/* eslint-disable no-console */
+import { initializeTabs } from '@chrome-extension-boilerplate/shared';
 import { exampleThemeStorage } from '@chrome-extension-boilerplate/storage';
 
-export async function toggleTheme() {
+export async function toggleTheme(): Promise<void> {
   console.log('initial theme:', await exampleThemeStorage.get());
+  console.log('all the tabs, yay!: ', await initializeTabs());
   await exampleThemeStorage.toggle();
   console.log('toggled theme:', await exampleThemeStorage.get());
 }
